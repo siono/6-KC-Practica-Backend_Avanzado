065cb118af0cdc47ca6838b5b19838ce7d0ad775
José Carlos <jc.esteban.gomez@gmail.com>
2017-11-01 20:59:56 +0100

    Añadidos cambios menores

--- nodepop/app.js
+++ nodepop/app.js
@@ -43,13 +43,30 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+
+  // establezco el status a la respuesta
+  err.status = err.status || 500;
+  res.status(err.status);
+
+  // si es un 500 lo pinto en el log
+  if (err.status && err.status >= 500) console.error(err);
+  
+  // si es una petición al API respondo JSON...
+  if (isAPI(req)) {
+    res.json({ success: false, error: err.message });
+    return;
+  }
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
   res.render('error');
 });
+
+function isAPI(req) {
+  return req.originalUrl.indexOf('/api') === 0;
+}
 
 module.exports = app;
